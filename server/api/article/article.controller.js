af2b29cf8a85f5fe660ab1bc2c9d6492ab318fef
Migi Domingo <migi.domingo@gmail.com>
2014-09-08 18:49:18 -0700

    scrape title and byline

--- server/api/article/article.controller.js
+++ server/api/article/article.controller.js
@@ -6,7 +6,29 @@ var cheerio = require('cheerio');
 
 // scrapes the article off of the URL then sends it back to the client
 exports.index = function(req, res) {
-	console.log(req.url);
+	console.log(req.body.url);
+	// start scraper and scrape article content
+	// article = {title: title, summary: summary, body: body}
+	// res.json(article);
+	var options = {
+		url: req.body.url,
+		method: 'GET'
+	};
+	// extract postNum
+	var articleObj = {};
+	console.log("here");
+	request(options, function(err, response, html){
+		if (!err) {
+			var $ = cheerio.load(html);
+			articleObj.title = $('h1.title').text();
+			articleObj.byline = $('header div a.navigate', '#page-body-content').text()
+			console.log(articleObj);
+			// get content of each .prose p
+		} else {
+			console.log(err);
+		}
+	});
+	console.log(articleObj);
 	res.send(200);
 };
 
