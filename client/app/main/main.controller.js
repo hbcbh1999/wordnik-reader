d14ffff9538abc8d8bd653e3a7be9ed5aeb07f93
Migi Domingo <migi.domingo@gmail.com>
2014-09-10 15:36:29 -0700

    implement enter binding

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,26 +4,42 @@ angular.module('wordnikReaderApp')
   .controller('MainCtrl', function ($scope, $http) {
   	$scope.show = false;
 
+		$scope.enter = function(e){
+			if (e.keyCode != 13){
+				return;
+			} else {
+				$scope.getArticle($scope.url);
+			}
+		}
 		$scope.getArticle = function(url){
-			// verify URL
-			// alert for non FC articles
-			// enter binding
 			$scope.wordObj = '';
-			var articleUrl = {url: url};
-			console.log(articleUrl);
-			$http.post("/api/articles", articleUrl)
-				.success(function(article){
-					console.log(article);
-					$scope.title = article.title.split(' ');
-					$scope.summary = article.summary.split(' ');
-					$scope.byline = article.byline;
-					$scope.content = article.content;
-					$scope.show = true;
-					$scope.url = "";
-				})
-				.error(function(error){
-					console.log(error);
-				});
+			var splitUrl = url.split(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/);
+			if (splitUrl.length === 1){
+				return alert('Please enter a Fast Company URL');
+			} else {
+				var sites = ['fastcompany', 'fastcoexist', 'fastcodesign', 'fastcolabs', 'fastcocreate'];
+				var baseUrl = splitUrl[3].split('.')[1];
+				console.log(baseUrl);
+				if (sites.indexOf(baseUrl)===-1){
+					return alert('Please enter a Fast Company URL')
+				} else {
+					var articleUrl = {url: url};
+					console.log(articleUrl);
+					$http.post("/api/articles", articleUrl)
+						.success(function(article){
+							console.log(article);
+							$scope.title = article.title.split(' ');
+							$scope.summary = article.summary.split(' ');
+							$scope.byline = article.byline;
+							$scope.content = article.content;
+							$scope.show = true;
+							$scope.url = "";
+						})
+						.error(function(error){
+							console.log(error);
+						});
+				};
+			};
 		};
 
 		$scope.changeShow = function(){
